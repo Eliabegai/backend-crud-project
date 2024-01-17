091f9ca76aa41de6bbdcf3c7e5b603eb730539d1
Eliabe Gai <eliabe.gai@clinicorp.com>
2024-01-16 22:49:12 -0300

    remove this.address()

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, process.env.PORT)
+    console.log(`Express server listening on port ${process.env.PORT || 3000}`)
   });
 }
 bootstrap();
