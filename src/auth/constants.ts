9f81b83e5a7b7bd41de876e5649d18adf12a0d27
Eliabe Gai <eliabe.gai@clinicorp.com>
2024-02-03 14:54:53 -0300

    feat: add jwt in login

---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  secret:
+    'UMA FRASE GRANDE PARA PODER USAR AQUI COM CACHORRO PORTA AZUL MACARRAO',
+};
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+
+export type User = any;
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: "eliabegai",
+      password: "12345",
+      role: "user",
+    },
+    {
+      userId: 2,
+      username: "admin",
+      password: "admin",
+      role: "admin",
+    },
+    {
+      userId: 3,
+      username: "john",
+      password: "changeme",
+      role: "user",
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
